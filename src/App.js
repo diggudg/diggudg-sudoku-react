451ff51b25825479a5689c1f578d4bca8cfbc931
Digvijay <Singh>
2020-07-20 16:04:31 +0200

    Updated folder with react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="/docs/5.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy" />
+              DigguDg Sudoku
+        </a>
+        </div>
+      </nav>
+    </React.Fragment>
+
+  );
+}
+
+export default App;
---
+++ src/grid/Grid.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class Grid extends Component {
+
+    render() {
+        return (<React.Fragment>
+            <div>Grid</div>
+        </React.Fragment>);
+    }
+}
